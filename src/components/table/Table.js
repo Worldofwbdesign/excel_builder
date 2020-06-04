9bb317f2a3d6b2522fd42ce6168ef4136a062dd7
Roman Martynenko <rmartynenko@deloitte.com>
2020-06-04 23:55:59 +0300

    Added table template

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,4 +1,5 @@
 import { ExcelComponent } from '@core/ExcelComponent'
+import { createTable } from './table.template'
 
 export class Table extends ExcelComponent {
   constructor(...props) {
@@ -7,24 +8,6 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return `
-      <div class="row">
-        <div class="row-info"></div>
-        <div class="row-data">
-          <div class="column">A</div>
-          <div class="column">B</div>
-          <div class="column">C</div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="row-info">1</div>
-        <div class="row-data">
-          <div class="cell -selected" contenteditable>A1</div>
-          <div class="cell" contenteditable>B1</div>
-          <div class="cell" contenteditable>C1</div>
-        </div>
-      </div>
-    `
+    return createTable()
   }
 }
---
+++ src/components/table/table.template.js
@@ -0,0 +1,41 @@
+const CODES = {
+  A: 65,
+  Z: 90
+}
+
+const createCell = content => `<div class="cell" contenteditable>${content}</div>`
+
+const createColumn = content => `<div class="column">${content}</div>`
+
+const createRow = (data, info = '') => `
+  <div class="row">
+    <div class="row-info">${info}</div>
+    <div class="row-data">${data}</div>
+  </div>
+`
+
+const toChar = charCode => String.fromCharCode(charCode)
+
+export const createTable = (rowsCount = 15) => {
+  const colsCount = CODES.Z - CODES.A + 1
+  const rows = []
+
+  const headerCols = Array(colsCount)
+    .fill('')
+    .map((_, index) => toChar(CODES.A + index))
+    .map(createColumn)
+    .join('')
+
+  rows.push(createRow(headerCols))
+
+  for (let i = 1; i <= rowsCount; i++) {
+    const cols = Array(colsCount)
+      .fill('')
+      .map(createCell)
+      .join('')
+
+    rows.push(createRow(cols, i))
+  }
+
+  return rows.join('')
+}
